359445d5dff1f209fa5ea1e104aac85f30322472
nam <namvh3@vng.com.vn>
2019-01-22 09:17:45 +0700

    naf nis

---
+++ src/main/java/ChatUtils/ServerService.java
@@ -0,0 +1,41 @@
+package ChatUtils;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.ServerChannel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ServerService {
+    private final int port;
+
+    public ServerService(int port){
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new ServerService(8081).run();
+    }
+
+    public void run() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChatServerInitializer());
+
+            bootstrap.bind(port).sync().channel().closeFuture().sync();
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
