2504dbb550baf30369c135a2a8da3869b7a85435
nam <namvh3@vng.com.vn>
2019-01-28 15:49:37 +0700

    developing feature

---
+++ src/main/java/server/api/RouterAPI.java
@@ -0,0 +1,35 @@
+package server.api;
+
+import org.apache.log4j.Logger;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+
+import static server.api.Login.*;
+import static server.api.Register.handleRegister;
+
+public class RouterAPI {
+
+    public static void router(ChannelHandlerContext context, HttpRequest request, Logger logger){
+
+        String uriAPI = request.getUri();
+        System.out.println(uriAPI);
+
+
+        switch (uriAPI) {
+            case  "/getListConversations":
+                getListConversations(context, request, logger);
+                break;
+            case "/getListMessagesByConversations":
+                getListMessagesByConversations(context, request, logger);
+                break;
+            case "/login":
+                handleLogin(context, request, logger);
+                break;
+            case "/register":
+                handleRegister(context, request, logger);
+                break;
+        }
+
+    }
+}
+
